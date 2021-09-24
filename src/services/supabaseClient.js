b0fcb7daa604060fad58328d55708cfd136db766
Eduardo Guette <61714979+eduardoguette@users.noreply.github.com>
2021-09-25 01:48:12 +0200

    Add files via upload

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+
+const supabaseUrl = 'https://bwouemlrxohhiwwoxscj.supabase.co' 
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
